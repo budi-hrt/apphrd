9be0a45c89a686072273a755e46ce00e5a1d38a2
budi-hrt <budhi.hrt82@gmail.com>
2022-05-31 13:50:38 +0800

    v_main

--- application/views/templates/v_main.php
+++ application/views/templates/v_main.php
@@ -17,8 +17,8 @@
     <?php
     if (isset($list_css_plugin)) {
         foreach ($list_css_plugin as $list_css) { ?>
-            <!-- JS plugin -->
-            <link rel="stylesheet" href="<?= base_url('assets/css/' . $list_css); ?>" />
+    <!-- JS plugin -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/' . $list_css); ?>" />
     <?php
         }
     }
@@ -28,7 +28,8 @@
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/fonts.googleapis.com.css" />
 
     <!-- ace styles -->
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/ace.min.css" class="ace-main-stylesheet"
+        id="main-ace-style" />
 
     <!--[if lte IE 9]>
 			<link rel="stylesheet" href="assets/css/ace-part2.min.css" class="ace-main-stylesheet" />
@@ -56,8 +57,8 @@
 		<script src="assets/js/respond.min.js"></script>
 		<![endif]-->
     <script>
-        let base_url = '<?= base_url(); ?>';
-        let id_user = "<?= $user['id']; ?>";
+    let base_url = '<?= base_url(); ?>';
+    let id_user = "<?= $user['id']; ?>";
     </script>
 </head>
 
@@ -72,13 +73,15 @@
                     </small>
                 </a>
 
-                <button class="pull-right navbar-toggle navbar-toggle-img collapsed" type="button" data-toggle="collapse" data-target=".navbar-buttons,.navbar-menu">
+                <button class="pull-right navbar-toggle navbar-toggle-img collapsed" type="button"
+                    data-toggle="collapse" data-target=".navbar-buttons,.navbar-menu">
                     <span class="sr-only">Toggle user menu</span>
 
                     <img src="<?= base_url(); ?>assets/images/avatars/user.jpg" alt="Jason's Photo" />
                 </button>
 
-                <button class="pull-right navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#sidebar">
+                <button class="pull-right navbar-toggle collapsed" type="button" data-toggle="collapse"
+                    data-target="#sidebar">
                     <span class="sr-only">Toggle sidebar</span>
 
                     <span class="icon-bar"></span>
@@ -140,7 +143,8 @@
                                                             </div>
 
                                                             <div class="progress progress-mini">
-                                                                <div style="width:35%" class="progress-bar progress-bar-danger"></div>
+                                                                <div style="width:35%"
+                                                                    class="progress-bar progress-bar-danger"></div>
                                                             </div>
                                                         </a>
                                                     </li>
@@ -153,7 +157,8 @@
                                                             </div>
 
                                                             <div class="progress progress-mini">
-                                                                <div style="width:15%" class="progress-bar progress-bar-warning"></div>
+                                                                <div style="width:15%"
+                                                                    class="progress-bar progress-bar-warning"></div>
                                                             </div>
                                                         </a>
                                                     </li>
@@ -166,7 +171,8 @@
                                                             </div>
 
                                                             <div class="progress progress-mini progress-striped active">
-                                                                <div style="width:90%" class="progress-bar progress-bar-success"></div>
+                                                                <div style="width:90%"
+                                                                    class="progress-bar progress-bar-success"></div>
                                                             </div>
                                                         </a>
                                                     </li>
@@ -188,7 +194,8 @@
                                                 <ul class="dropdown-menu dropdown-navbar">
                                                     <li>
                                                         <a href="#">
-                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
+                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar.png"
+                                                                class="msg-photo" alt="Alex's Avatar" />
                                                             <span class="msg-body">
                                                                 <span class="msg-title">
                                                                     <span class="blue">Alex:</span>
@@ -205,7 +212,8 @@
 
                                                     <li>
                                                         <a href="#">
-                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
+                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar3.png"
+                                                                class="msg-photo" alt="Susan's Avatar" />
                                                             <span class="msg-body">
                                                                 <span class="msg-title">
                                                                     <span class="blue">Susan:</span>
@@ -222,7 +230,8 @@
 
                                                     <li>
                                                         <a href="#">
-                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
+                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar4.png"
+                                                                class="msg-photo" alt="Bob's Avatar" />
                                                             <span class="msg-body">
                                                                 <span class="msg-title">
                                                                     <span class="blue">Bob:</span>
@@ -239,7 +248,8 @@
 
                                                     <li>
                                                         <a href="#">
-                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar2.png" class="msg-photo" alt="Kate's Avatar" />
+                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar2.png"
+                                                                class="msg-photo" alt="Kate's Avatar" />
                                                             <span class="msg-body">
                                                                 <span class="msg-title">
                                                                     <span class="blue">Kate:</span>
@@ -256,7 +266,8 @@
 
                                                     <li>
                                                         <a href="#">
-                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar5.png" class="msg-photo" alt="Fred's Avatar" />
+                                                            <img src="<?= base_url(); ?>assets/images/avatars/avatar5.png"
+                                                                class="msg-photo" alt="Fred's Avatar" />
                                                             <span class="msg-body">
                                                                 <span class="msg-title">
                                                                     <span class="blue">Fred:</span>
@@ -288,7 +299,9 @@
 
                     <li class="light-blue dropdown-modal user-min">
                         <a data-toggle="dropdown" href="#" class="dropdown-toggle">
-                            <img class="nav-user-photo" src="<?= base_url(); ?>assets/images/employe/<?= $user['image']; ?>" alt="Jason's Photo" />
+                            <img class="nav-user-photo"
+                                src="<?= base_url(); ?>assets/images/employe/<?= $user['image']; ?>"
+                                alt="Jason's Photo" />
                             <span class="user-info">
                                 <small>Welcome,</small>
                                 <?= $user['name']; ?>
@@ -297,7 +310,8 @@
                             <i class="ace-icon fa fa-caret-down"></i>
                         </a>
 
-                        <ul class="user-menu dropdown-menu-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+                        <ul
+                            class="user-menu dropdown-menu-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
                             <li>
                                 <a href="#">
                                     <i class="ace-icon fa fa-cog"></i>
@@ -382,9 +396,9 @@
 
     <div class="main-container ace-save-state" id="main-container">
         <script type="text/javascript">
-            try {
-                ace.settings.loadState('main-container')
-            } catch (e) {}
+        try {
+            ace.settings.loadState('main-container')
+        } catch (e) {}
         </script>
 
         <!-- Star Load tempaltes v_topbar -->
@@ -421,12 +435,14 @@
 
                             <div class="pull-left width-50">
                                 <div class="ace-settings-item">
-                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off" />
+                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover"
+                                        autocomplete="off" />
                                     <label class="lbl" for="ace-settings-hover"> Submenu on Hover</label>
                                 </div>
 
                                 <div class="ace-settings-item">
-                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" autocomplete="off" />
+                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact"
+                                        autocomplete="off" />
                                     <label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
                                 </div>
 
@@ -484,8 +500,8 @@
 <script src="assets/js/jquery-1.11.3.min.js"></script>
 <![endif]-->
     <script type="text/javascript">
-        if ('ontouchstart' in document.documentElement) document.write(
-            "<script src='assets/js/jquery.mobile.custom.min.js'>" + "<" + "/script>");
+    if ('ontouchstart' in document.documentElement) document.write(
+        "<script src='assets/js/jquery.mobile.custom.min.js'>" + "<" + "/script>");
     </script>
     <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
 
@@ -494,8 +510,8 @@
     <?php
     if (isset($list_js_plugin)) {
         foreach ($list_js_plugin as $list_js) { ?>
-            <!-- JS plugin -->
-            <script src="<?= base_url('assets/js/' . $list_js); ?>"></script>
+    <!-- JS plugin -->
+    <script src="<?= base_url('assets/js/' . $list_js); ?>"></script>
     <?php
         }
     }
@@ -511,8 +527,8 @@
     <?php
     if (isset($app_js)) {
         foreach ($app_js as $app_js) { ?>
-            <!-- JS App -->
-            <script src="<?= base_url('assets/app/js/' . $app_js); ?>"></script>
+    <!-- JS App -->
+    <script src="<?= base_url('assets/app/js/' . $app_js); ?>"></script>
     <?php
         }
     }
@@ -523,89 +539,89 @@
 
 
     <script type="text/javascript">
-        jQuery(function($) {
-            var $sidebar = $('.sidebar').eq(0);
-            if (!$sidebar.hasClass('h-sidebar')) return;
+    jQuery(function($) {
+        var $sidebar = $('.sidebar').eq(0);
+        if (!$sidebar.hasClass('h-sidebar')) return;
 
-            $(document).on('settings.ace.top_menu', function(ev, event_name, fixed) {
-                if (event_name !== 'sidebar_fixed') return;
+        $(document).on('settings.ace.top_menu', function(ev, event_name, fixed) {
+            if (event_name !== 'sidebar_fixed') return;
 
-                var sidebar = $sidebar.get(0);
-                var $window = $(window);
+            var sidebar = $sidebar.get(0);
+            var $window = $(window);
 
-                //return if sidebar is not fixed or in mobile view mode
-                var sidebar_vars = $sidebar.ace_sidebar('vars');
-                if (!fixed || (sidebar_vars['mobile_view'] || sidebar_vars['collapsible'])) {
-                    $sidebar.removeClass('lower-highlight');
-                    //restore original, default marginTop
-                    sidebar.style.marginTop = '';
+            //return if sidebar is not fixed or in mobile view mode
+            var sidebar_vars = $sidebar.ace_sidebar('vars');
+            if (!fixed || (sidebar_vars['mobile_view'] || sidebar_vars['collapsible'])) {
+                $sidebar.removeClass('lower-highlight');
+                //restore original, default marginTop
+                sidebar.style.marginTop = '';
 
-                    $window.off('scroll.ace.top_menu')
-                    return;
+                $window.off('scroll.ace.top_menu')
+                return;
+            }
+
+
+            var done = false;
+            $window.on('scroll.ace.top_menu', function(e) {
+
+                var scroll = $window.scrollTop();
+                scroll = parseInt(scroll /
+                    4); //move the menu up 1px for every 4px of document scrolling
+                if (scroll > 17) scroll = 17;
+
+
+                if (scroll > 16) {
+                    if (!done) {
+                        $sidebar.addClass('lower-highlight');
+                        done = true;
+                    }
+                } else {
+                    if (done) {
+                        $sidebar.removeClass('lower-highlight');
+                        done = false;
+                    }
                 }
 
+                sidebar.style['marginTop'] = (17 - scroll) + 'px';
+            }).triggerHandler('scroll.ace.top_menu');
 
-                var done = false;
-                $window.on('scroll.ace.top_menu', function(e) {
+        }).triggerHandler('settings.ace.top_menu', ['sidebar_fixed', $sidebar.hasClass('sidebar-fixed')]);
 
-                    var scroll = $window.scrollTop();
-                    scroll = parseInt(scroll /
-                        4); //move the menu up 1px for every 4px of document scrolling
-                    if (scroll > 17) scroll = 17;
-
-
-                    if (scroll > 16) {
-                        if (!done) {
-                            $sidebar.addClass('lower-highlight');
-                            done = true;
-                        }
-                    } else {
-                        if (done) {
-                            $sidebar.removeClass('lower-highlight');
-                            done = false;
-                        }
-                    }
-
-                    sidebar.style['marginTop'] = (17 - scroll) + 'px';
-                }).triggerHandler('scroll.ace.top_menu');
-
-            }).triggerHandler('settings.ace.top_menu', ['sidebar_fixed', $sidebar.hasClass('sidebar-fixed')]);
-
-            $(window).on('resize.ace.top_menu', function() {
-                $(document).triggerHandler('settings.ace.top_menu', ['sidebar_fixed', $sidebar.hasClass(
-                    'sidebar-fixed')]);
-            });
-
-
+        $(window).on('resize.ace.top_menu', function() {
+            $(document).triggerHandler('settings.ace.top_menu', ['sidebar_fixed', $sidebar.hasClass(
+                'sidebar-fixed')]);
         });
+
+
+    });
     </script>
 
 
 
     <script>
-        $('#skin-colorpicker').on('change', function() {
-            // const id_user = $('input[name=id_user]').val();
-            const val_skin = $('#skin-colorpicker').find(':selected').val();
-            let skin = '';
-            if (val_skin == '#438EB9') {
-                skin = 'no-skin'
-            } else if (val_skin == '#222A2D') {
-                skin = 'skin-1'
-            } else if (val_skin == '#C6487E') {
-                skin = 'skin-2'
-            } else if (val_skin == '#D0D0D0') {
-                skin = 'skin-3'
+    $('#skin-colorpicker').on('change', function() {
+        // const id_user = $('input[name=id_user]').val();
+        const val_skin = $('#skin-colorpicker').find(':selected').val();
+        let skin = '';
+        if (val_skin == '#438EB9') {
+            skin = 'no-skin'
+        } else if (val_skin == '#222A2D') {
+            skin = 'skin-1'
+        } else if (val_skin == '#C6487E') {
+            skin = 'skin-2'
+        } else if (val_skin == '#D0D0D0') {
+            skin = 'skin-3'
+        }
+        console.log(id_user);
+        $.ajax({
+            type: 'post',
+            url: base_url + 'admin/skin',
+            data: {
+                skin: skin,
+                id_user: id_user
             }
-            console.log(id_user);
-            $.ajax({
-                type: 'post',
-                url: base_url + 'admin/skin',
-                data: {
-                    skin: skin,
-                    id_user: id_user
-                }
-            });
         });
+    });
     </script>
 
 
